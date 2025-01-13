917aa951185357461d414a6db84f85255f8c966c
Kennedysenyo <kennedysenyo@gmail.com>
2025-01-13 19:06:01 +0000

    Initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import bgImg from "../assets/images/about-hero";
+
+const About = () => {
+  return(
+    <h1>About Page</h1>
+  )
+}
+
+export default About;
